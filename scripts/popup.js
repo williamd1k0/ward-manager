3e5c919a482e9ca4b8d389c6925eaf2b3ce5066d
William Claude Tumeo <tumeowilliam@gmail.com>
2015-10-28 02:38:10 -0200

    Update functions, add backup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,11 @@
 var wardTerm = document.querySelector('input[name="ward-term"]');
 var saveWard = document.querySelector('a[name="save"]');
 var queryWard = document.querySelector('a[name="query"]');
+var deleteWard = document.querySelector('a[name="delete"]');
+var backupWard = document.querySelector('a[name="backup"]');
+var refreshWard = document.querySelector('a[name="refresh"]');
+var closeWard = document.querySelector('a[name="close"]');
+var backupArea = document.querySelector('textarea');
 var dataBase = document.querySelector('datalist');
 
 var loadData = {};
@@ -24,9 +29,42 @@ saveWard.onclick = function(){
 };
 
 queryWard.onclick = function(){
-    var newWard = new Ward(wardTerm.value);
-    ward(newWard.getCode());
+    if(wardTerm.value){
+        var newWard = new Ward(wardTerm.value);
+        ward(newWard.getCode());
+    }
 };
+
+deleteWard.onclick = function(){
+    if(wardTerm.value){
+        delete loadData[wardTerm.value];
+        localStorage.setItem('data_base', JSON.stringify(loadData));
+        wardTerm.value = "";
+        wardTerm.placeholder = "Deletado";
+    }
+}
+
+backupWard.onclick = function(){
+    if(localStorage.data_base && !wardTerm.value){
+        backupArea.value = localStorage.data_base;
+        backupArea.style.display = 'block';
+    }else if(wardTerm.value){
+        try{
+            var insertBackup = JSON.parse(wardTerm.value);
+            localStorage.setItem('data_base', JSON.stringify(insertBackup));
+        }catch(e){
+            alert("Erro no json");
+        }
+    }
+}
+
+refreshWard.onclick = function(){
+    location.reload();
+}
+
+closeWard.onclick = function(){
+    window.close();
+}
 
 function showCode(code){
     wardTerm.readOnly = true;
